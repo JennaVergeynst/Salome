20b8c5aa458b60734992f3b78cf33fac2823a3e5
JennaVergeynst <jenna.vergeynst@ugent.be>
2016-08-22 16:46:11 +0200

    inlets ok
    Inlet faces detected

--- Ham_NS_MS.py.py
+++ Ham_NS_MS.py.py
@@ -64,31 +64,31 @@ height_box3 = -7
 L_channel = 100
 
 # z (depth) coordinates of the intersecting horizontal planes
-upper_DV = -0.75
-middle_NS_MS = -1.39
-lower_all = -4.35
+upper_DV = -0.75 # upper level of DV intake
+middle_NS_MS = -1.39 # upper level of NS and MS intake
+lower_all = -4.35 # lower level of all intakes
 sluice_bottom = -5
 # another horizontal plane needed to intersect at the point where the angle of the RB changes
 triangle_cut = -4.65
 
 # x-coordinates of vertical planes intersecting for the inlet openings and sluice door
 # NS (inlet raster - sluice - inlet raster)
-x1 = 8.475
-x2 = x1 + 3.60
-x3 = 15.075
-x4 = x3 + 16
-x5 = x4 + 3
-x6 = x5 + 3.60
+x1 = -8.475
+x2 = x1 - 3.60
+x3 = -15.075
+x4 = x3 - 16
+x5 = x4 - 3
+x6 = x5 - 3.60
 # MS (inlet raster - sluice - inlest raster)
-x7 = x6 + 2.80
-x8 = x7 + 3.60
-x9 = 16 + 16 + 15.075
-x10 = x9 + 16
-x11 = x10 + 3 
-x12 = x11 + 3.60
-# DV (RB inlet raster)
-x13 = x12 + 3.10 
-x14 = x13 + 4.40
+x7 = x6 - 2.80
+x8 = x7 - 3.60
+x9 = -16 - 16 - 15.075
+x10 = x9 - 16
+x11 = x10 - 3 
+x12 = x11 - 3.60
+# DV (RB inlet raster) !!!dimensions probably not correct!!!
+x13 = x12 - 3.10 
+x14 = x13 - 4.40
 
 
 ##---------------##
@@ -129,7 +129,7 @@ all3D = geompy.MakePrismVecH(all2D, Vy, -L_channel)
 
 # intersect all horizontally by a plane to create inlet openings
 def hor_planes(zco):
-	return geompy.MakePlane(geompy.MakeVertex(0, 0, zco), Vy, 200)
+	return geompy.MakePlane(geompy.MakeVertex(0, 0, zco), Vz, 200)
 	
 upper_DV_intersect = hor_planes(upper_DV)
 middle_intersect = hor_planes(middle_NS_MS)
@@ -137,6 +137,8 @@ lower_intersect = hor_planes(lower_all)
 sluice_bottom_intersect = hor_planes(sluice_bottom)
 triangle_intersect = hor_planes(triangle_cut)
 all3D = geompy.MakePartition([all3D], [upper_DV_intersect, middle_intersect, lower_intersect, sluice_bottom_intersect, triangle_intersect])
+geompy.addToStudy(all3D ,"all3D")
+
 
 # intersect all vertically by a plane to create inlet openings
 
@@ -144,7 +146,7 @@ def vert_planes(xco):
 	plane = geompy.MakePlane(geompy.MakeVertex(xco, 0, 0), Vx, 200)
 	return plane
 intersect_list = []
-x_list = [-x1, -x2, -x3, -x4, -x5, -x6, -x7, -x8, -x9, -x10, -x11, -x12, -x13, -x14]
+x_list = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14]
 for x in x_list:
 	intersect_list.append(vert_planes(x))
 
@@ -152,8 +154,11 @@ all3D = geompy.MakePartition([all3D], intersect_list)
 geompy.addToStudy(all3D ,"all3D")
 
 # add simplified sluice forms
-NS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(-x3, 0, 0), geompy.MakeVertex(-x4, 11.70, sluice_bottom))
-MS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(-x9, 0, 0), geompy.MakeVertex(-x10, 11.70, sluice_bottom))
+NS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(x3, 0, 0), geompy.MakeVertex(x4, 11.70, sluice_bottom))
+MS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(x9, 0, 0), geompy.MakeVertex(x10, 11.70, sluice_bottom))
+geompy.addToStudy(NS, "NS")
+geompy.addToStudy(MS, "MS")
+
 
 compound = geompy.MakeCompound([all3D, NS, MS])
 compound = geompy.MakeGlueFaces(compound,10**-7,1)
@@ -161,20 +166,16 @@ geompy.addToStudy(compound, "compound")
 
 #
 
-# find faces to extrude in direction of lock doors (combination of 3 faces)
-opening_NS_1 = geompy.GetShapesOnPlaneWithLocation(all3D, geompy.ShapeType["FACE"], Vz, geompy.MakeVertex((x4-x3)/2, 0, 0), GEOM.ST_ON)
-opening_NS_2 = geompy.GetShapesOnPlaneWithLocation(all3D, geompy.ShapeType["FACE"], Vz, geompy.MakeVertex((x4-x3)/2, upper_DV+0.1, 0), GEOM.ST_ON)
-opening_NS_3 = geompy.GetShapesOnPlaneWithLocation(all3D, geompy.ShapeType["FACE"], Vz, geompy.MakeVertex((x4-x3)/2, middle_NS_MS+0.1, 0), GEOM.ST_ON)
+# locate faces to determine inlets and outlets
 
-#opening_NS_1 = geompy.GetFaceByPoint(compound, geompy.MakeVertex(x3, 0, 0), geompy.MakeVertex(x3, 5, 0), geompy.MakeVertex(x4, 0, 0), geompy.MakeVertex(x4, 5 0))
-#print(opening_NS_1)
+# faces below are the outlet faces (intake of the sluice filling)
+intake_NS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x1+0.5, 0, middle_NS_MS-0.5))
+geompy.addToStudy(intake_NS_RB, "intake_NS_RB")
+intake_NS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x5+0.5, 0, middle_NS_MS-0.5))
+intake_MS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x7+0.5, 0, middle_NS_MS-0.5))
+intake_MS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x11+0.5, 0, middle_NS_MS-0.5))
 
-# create a group of shapes lying on a box
-#group_NS = geompy.CreateGroup(all3D, geompy.ShapeType["FACE"])
-# add the shapes to the group
-#geompy.UnionList(group_NS, opening_NS_2)
-#geompy.UnionList(group_NS, [opening_NS_1, opening_NS_2, opening_NS_3])
-#geompy.addToStudyInFather(all3D, group_NS, "group_NS")
+
 
 #check if the generated shape is valid
 print("Checking whether the created shape is valid")
