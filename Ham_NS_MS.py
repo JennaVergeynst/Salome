84a7c9f2add65fb0d7ff4b04ee8a983997c5b6ff
JennaVergeynst <jenna.vergeynst@ugent.be>
2016-08-30 16:09:54 +0200

    geometry complete
    completed geometry departing from aerial view

--- Ham_NS_MS.py
+++ Ham_NS_MS.py
@@ -7,7 +7,7 @@ Notes on running this python script for salome automatically:
 in the bash run the command:
 	salome720 -t <pathToScript>
 Where -t = TUI only
-afterwards execute following command to kill all sleeping salome sessions
+afterwards eyecute following command to kill all sleeping salome sessions
 	salome720 --killall
 '''
 ####----------####
@@ -50,148 +50,202 @@ def PrintMeshInfo(theMesh):
 ##-----------##
 print ("Reading in constants ...")
 
-# x-coordinates of vertical planes intersecting for the inlet openings and sluice door
+# y-coordinates of vertical planes intersecting for the inlet openings and sluice door
 # NS (inlet raster - sluice - inlet raster)
-x1 = -8.475
-x2 = x1 - 3.60
-x3 = -15.075
-x4 = x3 - 16
-x5 = x4 - 3
-x6 = x5 - 3.60
+y1 = -3
+y2 = y1 - 3.60
+y3 = -9.6
+y4 = y3 - 16
+y5 = y4 - 3
+y6 = y5 - 3.60
 # MS (inlet raster - sluice - inlest raster)
-x7 = x6 - 2.80
-x8 = x7 - 3.60
-x9 = -16 - 16 - 15.075
-x10 = x9 - 16
-x11 = x10 - 3 
-x12 = x11 - 3.60
+y7 = y6 - 2.80
+y8 = y7 - 3.60
+y9 = y3 -16 - 16
+y10 = y9 - 16
+y11 = y10 - 3 
+y12 = y11 - 3.60
 # DV
-x16 = x10 - 39
-x14 = x16 + 4 #protruding corner
-x15 = x14 - 0.60
-x17 = x15 - 2*4.90
-x18 = x17 - 3.60
-x19 = x18 - 24
-x20 = x19 - 3.60
-x21 = x20 - 2*4.90
-x22 = x21 - 0.60
+y16 = y10 - 29
+y14 = y16 + 4 #protruding corner
+y15 = y14 - 0.60
+y17 = y15 - 2*4.90
+y18 = y17 - 3.60
+y19 = y18 - 24
+y20 = y19 - 3.60
+y21 = y20 - 2*4.90
+y22 = y21 - 0.60
 # tap
-x13 = x16 + 11
+y13 = y16 + 11
 
 # divide the front view in 4 parts: 2 triangles + 1 rectangle of MS and NS, 1 rectangle for protruding DV
 
-# left x coordinate of each part:
-L_box1 = -6.5
-L_box2 = -16.4
-L_box3 = x16
-L_box4 = x22
+"""
+this part may be deleted
+# left y coordinate of each part:
+L_boy2 = -10.9 #skipped L_boy1: on the ground
+L_boy3 = y16
+L_boy4 = y22
 
 # depth (z-co) of each part
-height_box1 = -4.65 #triangle
-height_box2 = -7
-height_box3 = -7
+height_boy1 = -4.6 #trapezium instead of triangle
+height_boy2 = -7
+height_boy3 = -7
 
 # considered lenght of the upstream channel part
 L_channel = 100
 
+"""
+
 # z (depth) coordinates of the intersecting horizontal planes
-z_DV_up = -0.75
-z_DV_down = 
+z_DV_up = -2.13
+z_DV_down = -7.13
 
 z_MS_NS_up = -1.39
-
 z_MS_NS_down = -4.35
 
-z_MS_NS_up = -1.39 # upper level of NS and MS intake
-lower_all = -4.35 # lower level of all intakes
 sluice_bottom = -5
-# another horizontal plane needed to intersect at the point where the angle of the RB changes
-triangle_cut = -4.65
 
+# some other y-coordinates
+y_NS_RB = -9.6
+y_NS_LB = y_NS_RB - 16
+y_MS_RB = y_NS_LB - 16
+y_MS_LB = y_MS_RB - 16
+y_DV_RR = y_MS_LB - 29
+y_DV_RB = y_DV_RR - 10
+y_DV_LB = y_DV_RB - 24
+y_LB = y_DV_LB - 14
 
+# some x-coordinates
+x_MS_NS_cove = -13.7 # cove = inham
+x_DV_extr = 71.65
+x_DV_cove1 = x_DV_extr - 4.7
+x_DV_cove2 = x_DV_cove1 - 9.4
 ##---------------##
 ## Geometry ##
 ##---------------##
 print ("Geometry construction ...")
 
-# make the workingplane of sketcher: the OZX plane (= 3)
-XZ_plane = geompy.MakeFaceHW(100, 500, 3)
+# 1. start the geometry from aerial view: first define all points
 
-# generation of front view NS and MS
-# MakeSketcher creates the edges, MakeFace fills them to a face
-# the origin is on the right bank of the channel
+p0 = geompy.MakeVertex(0, 0, 0)
+p1 = geompy.MakeVertex(0, y_NS_RB, 0)
+p2 = geompy.MakeVertex(-3, y_NS_RB, 0)
+p3 = geompy.MakeVertex(-4.08, y_NS_RB+1.28, 0)
+p4 = geompy.MakeVertex(x_MS_NS_cove, y_NS_RB+1.28, 0)
+p5 = geompy.MakeVertex(x_MS_NS_cove, y_NS_RB, 0)
+p6 = geompy.MakeVertex(-11.7, y_NS_RB - 8, 0)
+p7 = geompy.MakeVertex(x_MS_NS_cove, y_NS_LB, 0)
+p8 = geompy.MakeVertex(x_MS_NS_cove , y_NS_LB-1.28, 0)
+p9 = geompy.MakeVertex(-4.08, y_NS_LB-1.28, 0)
+p10 = geompy.MakeVertex(-3, y_NS_LB, 0)
+p11 = geompy.MakeVertex(0, y_NS_LB, 0)
 
-part1 = geompy.MakeSketcherOnPlane("Sketcher:F 0 0:TT 0 "+ str(L_box1) + ":TT " + str(height_box1)+ " " + str(L_box1)  + ":TT 0 0", XZ_plane)
-part1_2D = geompy.MakeFace(part1, 1)
-part2= geompy.MakeSketcherOnPlane("Sketcher:F 0 "+ str(L_box1) + " :TT "+ str(height_box1) + " " +str(L_box1) + ":TT "  + str(height_box2) + " " + str(L_box2)+ ":TT 0 " + str(L_box2) + ":TT 0 " + str(L_box1), XZ_plane)
-part2_2D = geompy.MakeFace(part2, 1)
-part3 = geompy.MakeSketcherOnPlane("Sketcher:F 0 "+ str(L_box2) + " :TT "+ str(height_box3) + " " +str(L_box2) + ":TT " + str(height_box3) + " " + str(L_box3) + ":TT 0 " + str(L_box3) + ":TT 0 " + str(L_box2), XZ_plane)
-part3_2D = geompy.MakeFace(part3, 1)
+p12 = geompy.MakeVertex(0, y_MS_RB, 0)
+p13 = geompy.MakeVertex(-3, y_MS_RB, 0)
+p14 = geompy.MakeVertex(-4.08, y_MS_RB+1.28, 0)
+p15 = geompy.MakeVertex(x_MS_NS_cove, y_MS_RB+1.28, 0)
+p16 = geompy.MakeVertex(x_MS_NS_cove, y_MS_RB, 0)
+p17 = geompy.MakeVertex(-11.7, y_MS_RB - 8, 0)
+p18 = geompy.MakeVertex(x_MS_NS_cove, y_MS_LB, 0)
+p19 = geompy.MakeVertex(x_MS_NS_cove , y_MS_LB-1.28, 0)
+p20 = geompy.MakeVertex(-4.08, y_MS_LB-1.28, 0)
+p21 = geompy.MakeVertex(-3, y_MS_LB, 0)
+p22 = geompy.MakeVertex(0, y_MS_LB, 0)
 
-# add all faces together (via partition, but nothing is cut out here)
-all2D = geompy.MakeCompound([part1_2D,part2_2D,part3_2D])
-all2D = geompy.RemoveExtraEdges(all2D, True)
+p23 = geompy.MakeVertex(0, y_DV_RR, 0)
+p24 = geompy.MakeVertex(46.55, y_DV_RR, 0)
+p25 = geompy.MakeVertex(51.55, y_DV_RR+5, 0)
+p26 = geompy.MakeVertex(x_DV_extr-6, y_DV_RR+5, 0) #6: estimated from map
+p27 = geompy.MakeVertex(x_DV_extr-4.7, y_DV_RB+14, 0)
+p28 = geompy.MakeVertex(x_DV_extr, y_DV_RB+14, 0)
 
+p29 = geompy.MakeVertex(x_DV_extr, y_DV_RB, 0)
+p30 = geompy.MakeVertex(x_DV_cove1, y_DV_RB, 0)
+p31 = geompy.MakeVertex(x_DV_cove1, y_DV_RB + 2.85, 0)
+p32 = geompy.MakeVertex(x_DV_cove2, y_DV_RB + 2.85, 0)
+p33 = geompy.MakeVertex(x_DV_cove2, y_DV_RB, 0)
+p34 = geompy.MakeVertex(x_DV_cove2, y_DV_LB, 0)
+p35 = geompy.MakeVertex(x_DV_cove2, y_DV_LB - 2.85, 0)
+p36 = geompy.MakeVertex(x_DV_cove1, y_DV_LB - 2.85, 0)
+p37 = geompy.MakeVertex(x_DV_cove1, y_DV_LB, 0)
+p38 = geompy.MakeVertex(x_DV_extr, y_DV_LB, 0)
 
-# create vectors along the axes
-Vx = geompy.MakeVectorDXDYDZ(1, 0, 0)
+p39 = geompy.MakeVertex(x_DV_extr, y_LB, 0)
+p44 = geompy.MakeVertex(x_DV_extr+218.875, y_DV_LB - 45, 0)
+p45 = geompy.MakeVertex(x_DV_extr+218.875, y_DV_LB + 135, 0)
+p46 = geompy.MakeVertex(120, y_DV_LB + 135, 0) #120 AANPASSEN NR WERKELIJKE X!!!
+
+# 2. make a closed polyline
+
+poly = geompy.MakePolyline([p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, 
+p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p44, p45, p46, p0])
+
+# 3. closed polyline becomes a face
+
+groundface = geompy.MakeFace(poly, isPlanarWanted = True)
+geompy.addToStudy(groundface, "groundface")
+
+# 4. extend the face in z-direction (depth)
+
+# create vectors along the ayes
+Vy = geompy.MakeVectorDXDYDZ(1, 0, 0)
 Vy = geompy.MakeVectorDXDYDZ(0, 1, 0)
 Vz = geompy.MakeVectorDXDYDZ(0, 0, 1)
 
-# rotate all2D 180 around z-axis
-all2D = geompy.MakeRotation(all2D, Vz, math.pi)
-geompy.addToStudy(all2D ,"all2D")
+shape3D = geompy.MakePrismVecH(groundface, Vz, -7)
+geompy.addToStudy(shape3D, "shape3D")
 
-# extrude the front view over the lenght of the study site
-all3D = geompy.MakePrismVecH(all2D, Vy, -L_channel)
+# 5. cut the 3D-shape by use of horizontal and vertical planes
 
 # intersect all horizontally by a plane to create inlet openings
 def hor_planes(zco):
-	return geompy.MakePlane(geompy.MakeVertex(0, 0, zco), Vz, 200)
+	return geompy.MakePlane(geompy.MakeVertex(0, 0, zco), Vz, 1000)
 	
 z_DV_up_intersect = hor_planes(z_DV_up)
 middle_intersect = hor_planes(z_MS_NS_up)
-lower_intersect = hor_planes(lower_all)
+lower_intersect = hor_planes(z_MS_NS_down)
 sluice_bottom_intersect = hor_planes(sluice_bottom)
-triangle_intersect = hor_planes(triangle_cut)
-all3D = geompy.MakePartition([all3D], [z_DV_up_intersect, middle_intersect, lower_intersect, sluice_bottom_intersect, triangle_intersect])
+all3D = geompy.MakePartition([shape3D], [z_DV_up_intersect, middle_intersect, lower_intersect, sluice_bottom_intersect])
 geompy.addToStudy(all3D ,"all3D")
-
 
 # intersect all vertically by a plane to create inlet openings
 
-def vert_planes(xco):
-	plane = geompy.MakePlane(geompy.MakeVertex(xco, 0, 0), Vx, 200)
+def vert_planes(yco):
+	plane = geompy.MakePlane(geompy.MakeVertex(0, yco, 0), Vy, 1000)
 	return plane
 intersect_list = []
-x_list = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14]
-for x in x_list:
-	intersect_list.append(vert_planes(x))
+y_list = [y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22]
+for y in y_list:
+	intersect_list.append(vert_planes(y))
 
 all3D = geompy.MakePartition([all3D], intersect_list)
 geompy.addToStudy(all3D ,"all3D")
 
-# add simplified sluice forms
-NS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(x3, 0, 0), geompy.MakeVertex(x4, 11.70, sluice_bottom))
-MS = geompy.MakeBoxTwoPnt(geompy.MakeVertex(x9, 0, 0), geompy.MakeVertex(x10, 11.70, sluice_bottom))
-geompy.addToStudy(NS, "NS")
-geompy.addToStudy(MS, "MS")
+# 6. locate faces to determine inlets and outlets (see old method)
 
 
-compound = geompy.MakeCompound([all3D, NS, MS])
-compound = geompy.MakeGlueFaces(compound,10**-7,1)
-geompy.addToStudy(compound, "compound")
+
+
+
+
+
+###------OLD METHODOLOGY-------
+"""
+
+
+
 
 #
 
 # locate faces to determine inlets and outlets
 
 # faces below are the outlet faces (intake of the sluice filling)
-intake_NS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x1+0.5, 0, z_MS_NS_up-0.5))
+intake_NS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertey(y1+0.5, 0, z_MS_NS_up-0.5))
 geompy.addToStudy(intake_NS_RB, "intake_NS_RB")
-intake_NS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x5+0.5, 0, z_MS_NS_up-0.5))
-intake_MS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x7+0.5, 0, z_MS_NS_up-0.5))
-intake_MS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertex(x11+0.5, 0, z_MS_NS_up-0.5))
+intake_NS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertey(y5+0.5, 0, z_MS_NS_up-0.5))
+intake_MS_RB = geompy.GetFaceNearPoint(compound, geompy.MakeVertey(y7+0.5, 0, z_MS_NS_up-0.5))
+intake_MS_LB = geompy.GetFaceNearPoint(compound, geompy.MakeVertey(y11+0.5, 0, z_MS_NS_up-0.5))
 
 
 
@@ -203,6 +257,6 @@ if IsValid == 0:
 else:
     print("Hurray! Created geometry is valid!")
 
-
+"""
 
 	
